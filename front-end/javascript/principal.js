3e7c7425d12de2bd80cd3d9e3e392881d555c437
Vitor Souza <156625682+vitor-souza17@users.noreply.github.com>
2024-01-21 15:21:57 -0300

    Adicionei novos arquivos do projeto.
    Adicionei a página de cadastro, bem como seus estilos com CSS e interações com JavaScript.

--- front-end/javascript/principal.js
+++ front-end/javascript/principal.js
@@ -1,7 +1,39 @@
 const botaoMenu = document.querySelector("#botaoMenu");
+const areaFundoModal = document.querySelector("#areaFundoModal");
+const botaoFecharModal = document.querySelector("#botaoFecharModal");
+
+// Adiciona evento "click" à janela do navegador
+window.addEventListener("click", fecharModalClique);
+
+// Adiciona evento "keydown" à janela do navegador
+window.addEventListener("keydown", fecharModalTecla);
 
 // Adiciona evento "click" ao botão de menu
 botaoMenu.addEventListener("click", mostrarBarraNav);
+
+// Adiciona evento "click" ao botão de fechar o modal
+botaoFecharModal.addEventListener("click", fecharModal);
+
+// Função que fecha o modal quando o usuário clica fora dele
+function fecharModalClique(event){
+	// Verifica se usuário clicou no elemento de id #areaFundoModal, ou seja, se usuário clicou fora dele
+	if(event.target === areaFundoModal){
+		fecharModal();
+	}
+}
+
+// Função que fecha o modal quando o usuário aperta "Esc"
+function fecharModalTecla(event){
+	// Verifica se #areaFundoModal tem a classe .ativo, ou seja, se está sendo exibido e se tecla pressionada foi "Esc"
+	if(areaFundoModal.classList.contains("ativo") && event.key === "Escape"){
+		fecharModal();
+	}
+}
+
+// Função que fecha o modal
+function fecharModal(){
+	areaFundoModal.classList.remove("ativo");
+}
 
 // Função que mostra e oculta a barra de navegação
 function mostrarBarraNav(){
